3b93c9bfb9e57cc7105b9f38abb1bb294f863f49
ana <114084376+ana-borowsky@users.noreply.github.com>
2025-09-12 12:26:13 -0300

    Refatora app.jsx e cria componente citymanagement

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,8 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import Form from "./components/Form.jsx";
-import Grid from "./components/Grid.jsx";
-import "./App.css";
-import "./components/Form.css";
-import "./components/Grid.css";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "./components/Home.jsx";
-
-const CityManagement = () => {
-  const [cities, setCities] = useState([]);
-  const [onEdit, setOnEdit] = useState(null);
-
-  const getCities = async () => {
-    try {
-      const res = await axios.get("http://localhost:8800/");
-      setCities(res.data.sort((a, b) => (a.name > b.name ? 1 : -1)));
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getCities(); cities
-  }, []);
-
-  return (
-    <>
-      <div className="container">
-        <h1>Gerencie as Cidades</h1>
-        <Form onEdit={onEdit} setOnEdit={setOnEdit} getCities={getCities} />
-        <Grid cities={cities} setCities={setCities} setOnEdit={setOnEdit} />
-      </div>
-    </>
-  );
-};
+import CityManagement from "./components/CityManagement.jsx";
+import "./App.css";
 
 const App = () => {
   return (
