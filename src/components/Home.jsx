c75b3de14d72f6dac0f0dde179c75beb0b0ee852
ana <114084376+ana-borowsky@users.noreply.github.com>
2025-09-13 20:22:38 -0300

    Adiciona mensagem de cidade não encontrada

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -93,13 +93,18 @@ const HomePage = () => {
           ))}
         </div>
       ) : (
-        <div className="sad-sun-container">
-          <img
-            src="/assets/sad_sun.svg"
-            alt="Sad sun - no cities found"
-            className="sad-sun-icon"
-          />
-        </div>
+          <div className="sad-sun-container">
+            <img
+              src="/assets/sad_sun.svg"
+              alt="Sad sun - no cities found"
+              className="sad-sun-icon"
+            />
+            <p className="smaller">
+              <strong>
+              Try searching for another city or add it to the database.
+              </strong>
+            </p>
+          </div>
       )}
 
       {toast && (
